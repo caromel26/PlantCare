a451ae7e757d3cbf3f5e9877925bd9854f1e3f38
Karolina Poręba <kjporeba@student.wsb-nlu.edu.pl>
2024-06-03 23:07:58 +0200

    Create project files

---
+++ PlantCare.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PlantCare.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ PlantCare.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PlantCare.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
