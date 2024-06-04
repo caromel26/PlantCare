3c1bfbcf2d58d64e26219f022315833bdda4b841
Karolina Poręba <kjporeba@student.wsb-nlu.edu.pl>
2024-06-04 13:12:08 +0200

    Create models

---
+++ PlantCare.Mobile/Models/PlantTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlantCare.Mobile.Models.Abstractions;
+
+namespace PlantCare.Mobile.Models
+{
+    public class PlantTask : BaseModel
+    {
+        public int PlantId { get; set; }
+        public int TaskTypeId { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool CompletionStatus { get; set; }
+    }
+}
---
+++ PlantCare.Mobile/Models/Reminder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlantCare.Mobile.Models.Abstractions;
+
+namespace PlantCare.Mobile.Models
+{
+    public class Reminder : BaseModel
+    {
+        public int PlantId { get; set; }
+        public DateTime ReminderDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
